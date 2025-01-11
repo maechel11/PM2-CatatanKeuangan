d260916294dde33c002c27c9a35b6c1e01784e07
jhoshemaechel01 <jhoshemaechel01@gmail.com>
2025-01-11 10:17:43 +0000

    memperbagus tampilan tambah catatan

--- lib/app/modules/home/views/home_add_view.dart
+++ lib/app/modules/home/views/home_add_view.dart
@@ -10,101 +10,140 @@ class HomeAddView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tambah catatan'),
+        backgroundColor: const Color.fromARGB(255, 255, 251, 218),
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+        shadowColor: const Color.fromARGB(255, 0, 0, 0).withOpacity(1),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: controller.cCatatan,
-              autocorrect: false,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(labelText: "Catatan"),
-            ),
-            const SizedBox(height: 10),
-            Obx(() {
-              return DropdownButtonFormField<String>(
-                value: controller.selectedJenis.value.isEmpty
-                    ? null
-                    : controller.selectedJenis.value,
-                onChanged: (value) {
-                  controller.selectedJenis.value = value ?? '';
-                },
-                decoration: const InputDecoration(
-                  labelText: "Jenis",
-                ),
-                items: const [
-                  DropdownMenuItem(
-                    value: "pemasukan",
-                    child: Text("Pemasukan"),
+      body: Container(
+        color: const Color.fromARGB(255, 255, 251, 218), // Warna latar belakang
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller.cCatatan,
+                autocorrect: false,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "Catatan",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  DropdownMenuItem(
-                    value: "pengeluaran",
-                    child: Text("Pengeluaran"),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Obx(() {
+                return DropdownButtonFormField<String>(
+                  value: controller.selectedJenis.value.isEmpty
+                      ? null
+                      : controller.selectedJenis.value,
+                  onChanged: (value) {
+                    controller.selectedJenis.value = value ?? '';
+                  },
+                  decoration: InputDecoration(
+                    labelText: "Jenis",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  items: const [
+                    DropdownMenuItem(
+                      value: "pemasukan",
+                      child: Text("Pemasukan"),
+                    ),
+                    DropdownMenuItem(
+                      value: "pengeluaran",
+                      child: Text("Pengeluaran"),
+                    ),
+                  ],
+                );
+              }),
+              const SizedBox(height: 10),
+              TextField(
+                controller: controller.cNominal,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: "Nominal (angka)",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Obx(() {
+                    return Text(
+                      controller.selectedTanggal.value.isEmpty
+                          ? "Pilih tanggal"
+                          : controller.selectedTanggal.value,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: controller.selectedTanggal.value.isEmpty
+                            ? const Color.fromARGB(255, 9, 9, 9)
+                            : Colors.black,
+                      ),
+                    );
+                  }),
+                  const SizedBox(
+                      width: 1), // Memberi jarak antara teks dan ikon
+                  IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2100),
+                      );
+                      if (pickedDate != null) {
+                        controller.selectedTanggal.value =
+                            pickedDate.toIso8601String().split('T')[0];
+                      }
+                    },
                   ),
                 ],
-              );
-            }),
-            const SizedBox(height: 10),
-            TextField(
-              controller: controller.cNominal,
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(labelText: "Nominal (angka)"),
-            ),
-            const SizedBox(height: 10),
-            Obx(() {
-              return TextField(
-                readOnly: true,
-                controller: TextEditingController(
-                  text: controller.selectedTanggal.value,
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange, // Warna tombol konfirmasi
                 ),
-                decoration: const InputDecoration(
-                  labelText: "Tanggal (YYYY-MM-DD)",
-                ),
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                  );
-                  if (pickedDate != null) {
-                    controller.selectedTanggal.value =
-                        pickedDate.toIso8601String().split('T')[0];
+                onPressed: () {
+                  final nominal = double.tryParse(controller.cNominal.text);
+                  final tanggal =
+                      DateTime.tryParse(controller.selectedTanggal.value);
+
+                  if (controller.selectedJenis.value.isEmpty ||
+                      nominal == null ||
+                      tanggal == null) {
+                    Get.snackbar(
+                      "Input tidak valid",
+                      "Pastikan semua field terisi dengan benar.",
+                      snackPosition: SnackPosition.BOTTOM,
+                    );
+                    return;
                   }
-                },
-              );
-            }),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                final nominal = double.tryParse(controller.cNominal.text);
-                final tanggal = DateTime.tryParse(controller.selectedTanggal.value);
 
-                if (controller.selectedJenis.value.isEmpty ||
-                    nominal == null ||
-                    tanggal == null) {
-                  Get.snackbar(
-                    "Input tidak valid",
-                    "Pastikan semua field terisi dengan benar.",
-                    snackPosition: SnackPosition.BOTTOM,
+                  controller.add(
+                    controller.cCatatan.text,
+                    controller.selectedJenis.value,
+                    nominal,
+                    Timestamp.fromDate(tanggal),
                   );
-                  return;
-                }
-
-                controller.add(
-                  controller.cCatatan.text,
-                  controller.selectedJenis.value,
-                  nominal,
-                  Timestamp.fromDate(tanggal),
-                );
-              },
-              child: const Text("Simpan"),
-            ),
-          ],
+                },
+                child: const Text("Simpan"),
+              ),
+            ],
+          ),
         ),
       ),
     );
